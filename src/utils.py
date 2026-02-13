e37137add646119f2d7d2d78d66914645e5c2377
OmKatiyar <katiyarom004@gmail.com>
2026-02-13 12:07:38 +0530

    Removed catboost_info folder

--- src/utils.py
+++ src/utils.py
@@ -8,44 +8,53 @@ from src.exception import CustomException
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
-def save_object(file_path,obj):
+
+def save_object(file_path, obj):
     try:
-        dir_path=os.path.dirname(file_path)
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
 
-        os.makedirs(dir_path,exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            dill.dump(obj, file_obj)
 
-        with open(file_path,"wb") as file_obj:
-            dill.dump(obj,file_obj)
     except Exception as e:
-        raise CustomException(e,sys)     
-    
-def evaluate_models(X_train,X_test,y_train,y_test,models,param):
+        raise CustomException(e, sys)
+
+
+def evaluate_models(X_train, X_test, y_train, y_test, models, param):
     try:
-        report ={}
+        report = {}
 
         for i in range(len(models)):
             model = list(models.values())[i]
             model_name = list(models.keys())[i]
-            para=param[list(models.keys())[i]]
+            para = param[model_name]
 
-            gs = GridSearchCV(model,para,cv=3)
-            gs.fit(X_train,y_train)
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, y_train)
 
             model.set_params(**gs.best_params_)
-            model.fit(X_train,y_train)
-
-    # Train
             model.fit(X_train, y_train)
 
-    # Predict
+            # Predict
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
-            train_model_score=r2_score(y_train,y_train_pred)
-            test_model_score = r2_score(y_test,y_test_pred)
-            
-            report[list(models.keys())[i]] = test_model_score
 
-            return report
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[model_name] = test_model_score
+
+        return report   # ✅ return after loop ends
 
     except Exception as e:
-        raise CustomException(e,sys) 
+        raise CustomException(e, sys)
+
+
+def load_object(file_path):   # ✅ FIXED INDENTATION
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
